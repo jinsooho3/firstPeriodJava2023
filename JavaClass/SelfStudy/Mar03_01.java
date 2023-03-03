4a88d8640d153f683e28f2d5dc9537f68f23d66c
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-03 20:28:52 +0900

    Create Mar03_01.java

---
+++ JavaClass/SelfStudy/Mar03_01.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int add(int arg1, int arg2){
+        return arg1 + arg2;
+    }
+}
+
+public class Mar03_01 {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.add(1, 2));
+    }
+}
