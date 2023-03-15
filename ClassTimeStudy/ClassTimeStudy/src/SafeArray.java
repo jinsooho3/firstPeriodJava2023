df68d400bd4abef3d5c6cd053920c029c00ab53e
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-15 20:14:31 +0900

    Create SafeArray.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/SafeArray.java
@@ -0,0 +1,30 @@
+public class SafeArray {
+    private int a[];
+    public int length;
+
+    public SafeArray(int size) {
+        a = new int[size];
+        length = size;
+    }
+
+    public int get(int index) {
+        if(index > 0 && index<length){
+            return a[index];
+        }
+        return -1;
+    }
+
+    public void put(int index, int value) {
+        if(index >= 0 && index < length){
+            a[index] = value;
+        }else{
+            System.out.println("잘못된 인덱스" + index);
+        }
+
+    }
+    public void getArray(){
+        for(int i = 0 ;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
