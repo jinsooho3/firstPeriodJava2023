ad9f2191943de5cc7aedddee63394d3aadb375e2
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-15 20:14:38 +0900

    Create Test.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        
+        for(int i = 0;i<=10;i++){
+            a[i] = i+1;
+        }
+    }
+}
