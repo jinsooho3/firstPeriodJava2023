5629afeb502c9abff7cd64eb41c4c6345105e85e
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-15 20:14:35 +0900

    Create Mar15Test.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/Mar15Test.java
@@ -0,0 +1,10 @@
+public class Mar15Test {
+    public static void main(String[] args) {
+        Mar15 mar = new Mar15();
+        mar.setName("Jinsoo");
+        mar.setBirthYear(1999);
+
+        System.out.println(mar.getName());
+        System.out.println(mar.getAge());
+    }
+}
