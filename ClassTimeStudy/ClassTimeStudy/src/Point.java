ee422e2c669bd3017f1360ba8b61b3a50e76aec5
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 15:44:22 +0900

    Create Point.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+    int x, y;
+
+    public Point(int a, int b){
+        x = a;
+        y = b;
+    }
+    public String toString() {
+        return "point [ x =" + x + ", y = " + y +" ]";
+    }
+}
