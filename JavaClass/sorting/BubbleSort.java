ed39441d99e1076fceb1db1b3f57d0cd3262a7bc
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 20:19:17 +0900

    Update BubbleSort.java

--- JavaClass/sorting/BubbleSort.java
+++ JavaClass/sorting/BubbleSort.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Random;
 
 public class BubbleSort {
@@ -8,7 +7,7 @@ public class BubbleSort {
         int[]wantToSortArray = new int[ArrayLength];
         // put  random number to wantToSortArray
         for(int i = 0;i<wantToSortArray.length;i++){
-            wantToSortArray[i] = rand.nextInt(100);
+            wantToSortArray[i] = rand.nextInt(100)+ 1;
         }
         // Just want to see random numbers  #not important
         for(int i = 0;i<wantToSortArray.length;i++){
@@ -16,8 +15,9 @@ public class BubbleSort {
         }
         System.out.println("-------------------------");
         //Sorting algorithm
-        for(int i = 0;i<wantToSortArray.length ;i++){
-            for(int j = 1;j<wantToSortArray.length;j++){
+        // 어차피 맨뒤로 가장큰수를 보냈으니까 맨뒤는 비교필요 X
+        for(int i = 5;i>=0 ;i--){
+            for(int j = 1;j<=i;j++){
                 if(wantToSortArray[j-1] > wantToSortArray[j]){
                     int temp;
                     temp = wantToSortArray[j-1];
@@ -32,3 +32,4 @@ public class BubbleSort {
         }
     }
 }
+// 이제 메소드 단위로 따개서 만들어보자 새로운 파일로
