287c947533f1fbaa932a380171372ded310e9bde
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 15:44:19 +0900

    Create RectangleTest.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/RectangleTest.java
@@ -0,0 +1,9 @@
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rtg = new Rectangle(5, 5);
+        rtg.getRectangle();
+
+        Rectangle rtg2 = new Rectangle(5, 10, 7, 8);
+        rtg2.getRectangle();
+    }
+}
