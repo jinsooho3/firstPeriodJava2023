e0309d82a8dbd790f3a1ad8b7cd0bbab5fd1eea5
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 15:44:14 +0900

    Create MyCounterTest.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/MyCounterTest.java
@@ -0,0 +1,9 @@
+public class MyCounterTest {
+    public static void main(String[] args) {
+        MyCounter obj1 = new MyCounter(100);
+        MyCounter obj2 = new MyCounter(200);
+
+        System.out.println("객체 1의 counter = " + obj1.counter);
+        System.out.println("객체 1의 counter = " + obj2.counter);        
+    }
+}
