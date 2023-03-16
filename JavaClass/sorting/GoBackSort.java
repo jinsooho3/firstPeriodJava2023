db250807303380bbe4227555add40483df07be32
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 20:19:19 +0900

    Create GoBackSort.java

---
+++ JavaClass/sorting/GoBackSort.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+public class GoBackSort {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] targetArray = new int[10]; // targetArray를 sort 하는 algorithm
+        int maxNumIndex;
+        for(int i = 0;i<targetArray.length;i++){ 
+            targetArray[i] = rand.nextInt(100) + 1; // 1~100 정수 랜덤
+        }
+        // 한눈에 보기 편하게 출력
+        // 보기 편한지는 모르겠음
+        for(int i = 0;i<targetArray.length;i++){
+            if(i != 0){
+                System.out.print(",");
+            }
+            System.out.print(targetArray[i]);
+        }
+        System.out.println();
+        for(int i = targetArray.length-1;i>=0;i--){
+            maxNumIndex = 0;
+            for(int j = 0;j<=i;j++){
+                if(targetArray[j]>targetArray[maxNumIndex]){
+                    maxNumIndex = j;
+                }
+            }
+            int temp = targetArray[maxNumIndex];
+            targetArray[maxNumIndex] = targetArray[i];
+            targetArray[i] = temp;
+        }
+        for(int i = 0;i<targetArray.length;i++){
+            if(i != 0){
+                System.out.print(",");
+            }
+            System.out.print(targetArray[i]);
+        }
+    }
+}
