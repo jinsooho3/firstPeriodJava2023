416ae5ec0d813d04d14ff74a66304316322ea764
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-16 15:44:16 +0900

    Create StudentTest.java

---
+++ ClassTimeStudy/ClassTimeStudy/src/StudentTest.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.WildcardType;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student std = new Student("jinsoo", 1999, "wd", "daegu");
+
+        std.getName();
+    }
+}
