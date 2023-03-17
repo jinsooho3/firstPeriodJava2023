7de027b53666f1d088dd9528d3f5f8c291e060d4
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-17 17:37:50 +0900

    Create Box.java

---
+++ ClassTime/Box.java
@@ -0,0 +1,29 @@
+
+
+public class Box {
+    private int width, length,height;
+    private int volume;
+    public Box(int w, int len, int h){
+        width = w;
+        length = len;
+        height = h;
+    }
+    public int getVolume(){
+        return width*height*length;
+    }
+
+    public static Box whoIsLargerBox(Box box1, Box box2){
+        if(box1.getVolume() > box2.getVolume()){
+            return box1;
+        }
+        return box2;
+    }
+
+    public static void main(String[] args) {
+        Box b1 = new Box(10, 20, 50);
+        Box b2 = new Box(10, 30, 30);
+        Box bigBox = Box.whoIsLargerBox(b1,b2);
+        System.out.println(bigBox.getVolume());
+       
+    }
+}
