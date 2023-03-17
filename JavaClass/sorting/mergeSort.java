c9090e626fcd6831dbb30f523f4e0c20bd9c0b18
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-17 17:45:21 +0900

    Update mergeSort.java

--- JavaClass/sorting/mergeSort.java
+++ JavaClass/sorting/mergeSort.java
@@ -13,9 +13,11 @@ public class mergeSort {
         int intMed = (int)med;
         System.out.println(intMed);
         System.out.println(args.length - intMed);
-        //중간지점에서 나누기
+        //중간지점부분으로 배열 나누기
         int[]firstArray = new int[intMed];
+        //남은부분도 배열로 나누기 즉 2등분하는것(홀수면 당연히 등분은 아님)
         int[]secondArray = new int[args.length-intMed];
+        //이걸 그 모든 원소가 1이될때까지 반복해야하는데 재귀함수 어떻게하더라
         for(int i = 0;i<args.length;i++){
             if(i<intMed){
                 firstArray[i] = targetArray[i];
@@ -24,6 +26,7 @@ public class mergeSort {
                 secondArray[i-intMed] = targetArray[i];
             }
         }
+
     }
     public static void main(String[] args) {
         mergeSort ms = new mergeSort();
