64943f897aea357f3ee16e3969d03badaa01a5e8
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-22 23:56:24 +0900

    Create NewMergeSort.java

---
+++ JavaClass/sorting/NewMergeSort.java
@@ -0,0 +1,43 @@
+public class NewMergeSort {
+
+    public static int[] src;
+    public static int[] tmp;
+    public static void main(String[] args) {
+        
+        src = new int[]{1, 9, 8, 5, 4, 2, 3, 7, 6};
+        tmp = new int[src.length];
+        printArray(src);
+        mergeSort(0, src.length-1);
+        printArray(src);
+    }
+
+    public static void mergeSort(int start, int end) {
+        if (start<end) {
+            int mid = (start+end) / 2;
+            mergeSort(start, mid);
+            mergeSort(mid+1, end);
+
+            int p = start;
+            int q = mid + 1;
+            int index = p;
+
+            while (p<=mid || q<=end) {
+                if (q>end || (p<=mid && src[p]<src[q])) {
+                    tmp[index++] = src[p++];
+                } else {
+                    tmp[index++] = src[q++];
+                }
+            }
+
+            for (int i=start;i<=end;i++) {
+                src[i]=tmp[i];
+            }
+        }
+    }
+
+    public static void printArray(int[] a) {
+        for (int i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
+        System.out.println();
+    }
+}
