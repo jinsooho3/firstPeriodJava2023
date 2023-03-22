896d435bfe291b4364201541614a3b35639704b5
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2023-03-22 17:52:24 +0900

    Update Car.java

--- JavaHomework/Car.java
+++ JavaHomework/Car.java
@@ -1,26 +1,26 @@
-public class Car {
-    String color; // 색
-    int gear; // 기어
-    int speed; // 속도
+// public class Car_1 {
+//     String color; // 색
+//     int gear; // 기어
+//     int speed; // 속도
   
-    // 기어 변경 함수 매개변수로 기어값 받기
-    void changeGear(int gear) {
-      gear = this.gear;
-    }
+//     // 기어 변경 함수 매개변수로 기어값 받기
+//     void changeGear(int gear) {
+//       gear = this.gear;
+//     }
   
-    // 속도 업 함수 속도 10 상승
-    void speedup() {
-      speed += 10;
-    }
+//     // 속도 업 함수 속도 10 상승
+//     void speedup() {
+//       speed += 10;
+//     }
   
-    // 속도 다운 함수 속도 10 감소
-    void speeddown() {
-      speed += 10;
-    }
+//     // 속도 다운 함수 속도 10 감소
+//     void speeddown() {
+//       speed += 10;
+//     }
   
-    // 오버라이딩을 통해 메소드를 재정의하여 원하는 값 출력
-    @Override
-    public String toString() {
-      return "Car [color=" + color + ", speed="+ speed + ", gear= "+ gear + "]";
-    }
-  }
+//     // 오버라이딩을 통해 메소드를 재정의하여 원하는 값 출력
+//     @Override
+//     public String toString() {
+//       return "Car [color=" + color + ", speed="+ speed + ", gear= "+ gear + "]";
+//     }
+//   }
